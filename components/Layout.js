456387cdd79a809f971ac22f4a0b017fc8d9a6bb
srinivas999 <villasrinivas9@gmail.com>
2026-04-17 21:28:18 +0530

    New header added and unnecessary content removed

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
+import { useRouter } from 'next/router';
 import Nav from './Nav';
 import Footer from './Footer';
 
 export default function Layout({ children }) {
+  const router = useRouter();
+  const hideHeader = router.pathname === '/ott-movies';
+
   return (
     <>
-      <Nav />
+      {!hideHeader && <Nav />}
       <main>{children}</main>
       <Footer />
     </>
