0a6d89eb76ef8126605ab219185d44af4bd6623d
srinivas999 <villasrinivas9@gmail.com>
2026-04-19 15:23:01 +0530

    Vercel analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../css/styles.css';
 import '../css/ott-movies.css';
+import { Analytics } from "@vercel/analytics/next"
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
